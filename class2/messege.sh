58062f69de4f90d9f8265d53be0fe767e8ae78d1
Tetiana2025 <bahlaitetiana@gmai.com>
2025-02-28 02:31:59 +0000

    class 2 files

---
+++ class2/messege.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+my_name=Tim
+fav_season=Fall
+fav_food=Burger
+
+echo "Hello, my name is $my_name, and my favoritefood is $fav_food, if your favor
+ite season is $fav_season, let's get along"
+
+
---
+++ class2/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+name=Tim
+
+echo "Hello my name is $name"
+echo "$name is currently studying Linux"
+echo "$name loves DevOps"
+
